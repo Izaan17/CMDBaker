46e72c85f4d38d2513349b6e7a2848a9580f645f
izaan.noman <izaan.nyc@gmail.com>
2024-10-31 19:50:25 -0400

    Change version constant

--- constants.py
+++ constants.py
@@ -5,4 +5,4 @@ USER = getpass.getuser()
 HOME_PATH = os.path.expanduser('~')
 FOLDER_LOCATION = os.path.join(HOME_PATH, 'CMDBaker')
 CONFIG_LOCATION = os.path.join(FOLDER_LOCATION, 'config.json')
-VERSION = 1.2
+VERSION = 1.3

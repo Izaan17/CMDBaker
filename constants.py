c7a31eac18fcd42bba78282eab88a974ef2723b1
izaan.noman <izaan.nyc@gmail.com>
2024-10-31 23:56:31 -0400

    Add version checking

--- constants.py
+++ constants.py
@@ -1,8 +1,24 @@
 import getpass
+import requests
 import os
+
+from utils.console import format_msg, MessageType
+
+
+def get_latest_version() -> float:
+    version_number = -1
+    try:
+        version = requests.get('https://raw.githubusercontent.com/Izaan17/CMDBaker/refs/heads/master/version.txt')
+        version_number = float(version.text)
+    except requests.exceptions.RequestException as request_error:
+        print(format_msg(MessageType.ERROR), f"Failed to get latest version: {request_error}")
+    except Exception as error:
+        print(format_msg(MessageType.ERROR), f"An unknown error occurred: {error}")
+
+    return version_number
 
 USER = getpass.getuser()
 HOME_PATH = os.path.expanduser('~')
 FOLDER_LOCATION = os.path.join(HOME_PATH, 'CMDBaker')
 CONFIG_LOCATION = os.path.join(FOLDER_LOCATION, 'config.json')
-VERSION = 1.3
+VERSION = get_latest_version()
